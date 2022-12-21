f6ebfa27ac79b1e31f40dec0be81edbfc2f4ff6b
Amit Yadav <amit@way2society.com>
2022-12-21 17:51:33 +0530

    Define Prefix and Action in constant file

--- CMMSAPIs/Helper/Constant.cs
+++ CMMSAPIs/Helper/Constant.cs
@@ -2,6 +2,16 @@
 {
     public class Constant
     {
+        // Token expiration time
         public const int TOKEN_EXPIRATION_TIME = 30;
+
+        // Possible actions in each modules
+        public const int CREATED = 1, UPDATED = 2, DELETED = 3, CANCELLED = 4, ASSIGNED = 5, ISSUED = 6, APPROVED = 7, REJECTED = 8;
+
+        // Module Prefix
+        public const string PREFIX_JOB = "JOB", PREFIX_PERMIT = "PERMIT", PREFIX_JC = "JC";
+
+
+        
     }
 }
