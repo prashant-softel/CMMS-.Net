6e3fddf9532d30482aa2ca7286a97301a445ff98
Amit Yadav <amit@way2society.com>
2023-02-11 18:04:12 +0530

    Audit module API definition

---
+++ CMMSAPIs/BS/Audits/AuditPlanBS.cs
@@ -0,0 +1,91 @@
+﻿using CMMSAPIs.Helper;
+using CMMSAPIs.Models.Audit;
+using CMMSAPIs.Models.Utils;
+using CMMSAPIs.Repositories.Audits;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CMMSAPIs.BS.Audits
+{
+    public interface IAuditPlanBS
+    {
+        Task<List<CMAuditPlan>> GetAuditPlanList(int facility_id);
+        Task<CMDefaultResponse> CreateAuditPlan(CMCreateAuditPlan request);
+        Task<CMDefaultResponse> UpdateAuditPlan(CMCreateAuditPlan request);
+        Task<CMDefaultResponse> DeleteAuditPlan(int audit_plan_id);
+    }
+    public class AuditPlanBS : IAuditPlanBS
+    {
+        private readonly DatabaseProvider databaseProvider;
+        private MYSQLDBHelper getDB => databaseProvider.SqlInstance();
+        public AuditPlanBS(DatabaseProvider dbProvider)
+        {
+            databaseProvider = dbProvider;
+        }
+
+        public async Task<List<CMAuditPlan>> GetAuditPlanList(int facility_id)
+        {
+            try
+            {
+                using (var repos = new AuditPlanRepository(getDB))
+                {
+                    return await repos.GetAuditPlanList(facility_id);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<CMDefaultResponse> CreateAuditPlan(CMCreateAuditPlan request)
+        {
+            try
+            {
+                using (var repos = new AuditPlanRepository(getDB))
+                {
+                    return await repos.CreateAuditPlan(request);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<CMDefaultResponse> UpdateAuditPlan(CMCreateAuditPlan request)
+        {
+            try
+            {
+                using (var repos = new AuditPlanRepository(getDB))
+                {
+                    return await repos.UpdateAuditPlan(request);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<CMDefaultResponse> DeleteAuditPlan(int audit_plan_id)
+        {
+            try
+            {
+                using (var repos = new AuditPlanRepository(getDB))
+                {
+                    return await repos.DeleteAuditPlan(audit_plan_id);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ CMMSAPIs/Controllers/Audits/AuditPlanController.cs
@@ -0,0 +1,85 @@
+﻿using CMMSAPIs.BS.Audits;
+using CMMSAPIs.Models.Audit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+
+namespace CMMSAPIs.Controllers.Audits
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditPlanController : ControllerBase
+    {
+        private readonly IAuditPlanBS _AuditPlanBS;
+
+        public AuditPlanController(IAuditPlanBS audit_plan)
+        {
+            _AuditPlanBS = audit_plan;
+        }
+
+        [Authorize]
+        [Route("GetAuditPlanList")]
+        [HttpGet]
+        public async Task<IActionResult> GetAuditPlanList(int facility_id)
+        {
+            try
+            {
+                var data = await _AuditPlanBS.GetAuditPlanList(facility_id);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [Authorize]
+        [Route("CreateAuditPlan")]
+        [HttpGet]
+        public async Task<IActionResult> CreateAuditPlan(CMCreateAuditPlan request)
+        {
+            try
+            {
+                var data = await _AuditPlanBS.CreateAuditPlan(request);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [Authorize]
+        [Route("UpdateAuditPlan")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateAuditPlan(CMCreateAuditPlan request)
+        {
+            try
+            {
+                var data = await _AuditPlanBS.UpdateAuditPlan(request);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [Authorize]
+        [Route("DeleteAuditPlan")]
+        [HttpGet]
+        public async Task<IActionResult> DeleteAuditPlan(int audit_plan_id)
+        {
+            try
+            {
+                var data = await _AuditPlanBS.DeleteAuditPlan(audit_plan_id);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
