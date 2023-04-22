406bdd18b0e48cf88d3fd6e9fb9a5ae4caa079e1
eeshandhuru <eeshan.s.dhuru@gmail.com>
2023-04-22 19:30:27 +0530

    Tested and made changes in Permit API's

--- CMMSAPIs/Models/Permits/CMUpdatePermit.cs
+++ CMMSAPIs/Models/Permits/CMUpdatePermit.cs
@@ -14,8 +14,8 @@ namespace CMMSAPIs.Models.Permits
         public int lotoId { get; set; }
         public int typeId { get; set; }
         public int facility_id { get; set; }
-        public DateTime start_date { get; set; }
-        public DateTime end_date { get; set; }
+        public DateTime? start_date { get; set; }
+        public DateTime? end_date { get; set; }
         public string description { get; set; }
         public int job_type_id { get; set; }
         public int sop_type_id { get; set; }
