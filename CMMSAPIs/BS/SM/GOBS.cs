0fc4e1e2c4c808b4dc5ba1b48097a05ff033e006
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2023-07-01 17:41:04 +0530

    Removed ambiguity in GO

--- CMMSAPIs/BS/SM/GOBS.cs
+++ CMMSAPIs/BS/SM/GOBS.cs
@@ -1,22 +1,30 @@
-﻿using CMMSAPIs.Helper;
+using CMMSAPIs.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CMMSAPIs.Repositories.GO;
-using CMMSAPIs.Models.GO;
+using CMMSAPIs.Repositories;
+using CMMSAPIs.Models;
 using CMMSAPIs.Models.Utils;
+using CMMSAPIs.Models.Users;
+using MailKit.Search;
 
-namespace CMMSAPIs.BS.GO
+namespace CMMSAPIs.BS
 {
     public interface IGOBS
     {
-        Task<List<CMGO>> GetGOList(int plantID, DateTime fromDate, DateTime toDate);
-        Task<List<CMGO>> GetAssetCodeDetails(int asset_code);
-        Task<CMDefaultResponse> CreateGO(CMGO request, int userID);
-        Task<CMDefaultResponse> UpdateGO(CMGO request, int userID);
+        Task<List<CMGoodsOrderDetailList>> GetGOList(int facility_id, DateTime fromDate, DateTime toDate, int Status);
+        Task<List<CMGoodsOrderList>> GetGOItemByID(int id);
+        Task<List<CMGoodsOrderList>> GetAssetCodeDetails(int asset_code);
+        Task<CMDefaultResponse> CreateGO(CMGoodsOrderList request, int userID);
+        Task<CMDefaultResponse> UpdateGO(CMGoodsOrderList request, int userID);
         Task<CMDefaultResponse> DeleteGO(int GOid, int userID);
-        Task<CMDefaultResponse> WithdrawGO(CMGO request, int userID);
+        Task<CMDefaultResponse> WithdrawGO(CMGoodsOrderList request, int userID);
+        Task<CMDefaultResponse> GOApproval(CMApproval request);
+        Task<CMDefaultResponse> RejectGO(CMApproval request);
+        Task<List<CMPURCHASEDATA>> GetPurchaseData(int plantID, string empRole, DateTime fromDate, DateTime toDate, string status, string order_type);
+        Task<CMGOMaster> getPurchaseDetailsByID(int id);
+        Task<CMDefaultResponse> SubmitPurchaseData(CMSUBMITPURCHASEDATA request);
     }
 
     public class GOBS : IGOBS
@@ -28,13 +36,13 @@ namespace CMMSAPIs.BS.GO
             databaseProvider = dbProvider;
         }
 
-        public async Task<List<CMGO>> GetGOList(int plantID, DateTime fromDate, DateTime toDate)
+        public async Task<List<CMGoodsOrderDetailList>> GetGOList(int facility_id, DateTime fromDate, DateTime toDate, int Status)
         {
             try
             {
                 using (var repos = new GORepository(getDB))
                 {
-                    return await repos.GetGOList(plantID, fromDate, toDate);
+                    return await repos.GetGOList(facility_id, fromDate, toDate);
 
                 }
             }
@@ -43,8 +51,23 @@ namespace CMMSAPIs.BS.GO
                 throw;
             }
         }
+        
+        public async Task<List<CMGoodsOrderList>> GetGOItemByID(int id)
+        {
+            try
+            {
+                using (var repos = new GORepository(getDB))
+                {
+                    return await repos.GetGOItemByID(id);
 
-        public async Task<List<CMGO>> GetAssetCodeDetails(int asset_code)
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task<List<CMGoodsOrderList>> GetAssetCodeDetails(int asset_code)
         {
             try
             {
@@ -60,7 +83,7 @@ namespace CMMSAPIs.BS.GO
             }
         }
 
-        public async Task<CMDefaultResponse> CreateGO(CMGO request, int userID)
+        public async Task<CMDefaultResponse> CreateGO(CMGoodsOrderList request, int userID)
         {
             try
             {
@@ -76,7 +99,7 @@ namespace CMMSAPIs.BS.GO
             }
         }
 
-        public async Task<CMDefaultResponse> UpdateGO(CMGO request, int userID)
+        public async Task<CMDefaultResponse> UpdateGO(CMGoodsOrderList request, int userID)
         {
             try
             {
@@ -107,13 +130,90 @@ namespace CMMSAPIs.BS.GO
                 throw;
             }
         }
-        public async Task<CMDefaultResponse> WithdrawGO(CMGO request, int userID)
+        public async Task<CMDefaultResponse> WithdrawGO(CMGoodsOrderList request, int userID)
         {
             try
             {
                 using (var repos = new GORepository(getDB))
                 {
                     return await repos.WithdrawGO(request, userID);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task<CMDefaultResponse> GOApproval(CMApproval request)
+        {
+            try
+            {
+                using (var repos = new GORepository(getDB))
+                {
+                    return await repos.ApproveGoodsOrder(request);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task<CMDefaultResponse> RejectGO(CMApproval request)
+        {
+            try
+            {
+                using (var repos = new GORepository(getDB))
+                {
+                    return await repos.RejectGoodsOrder(request);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<CMPURCHASEDATA>> GetPurchaseData(int plantID, string empRole, DateTime fromDate, DateTime toDate, string status, string order_type)
+        {
+            try
+            {
+                using (var repos = new GORepository(getDB))
+                {
+                    return await repos.GetPurchaseData(plantID, empRole, fromDate, toDate, status, order_type);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<CMGOMaster> getPurchaseDetailsByID(int id)
+        {
+            try
+            {
+                using (var repos = new GORepository(getDB))
+                {
+                    return await repos.getGoodsOrderDetailsByID(id);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task<CMDefaultResponse> SubmitPurchaseData(CMSUBMITPURCHASEDATA request)
+        {
+            try
+            {
+                using (var repos = new GORepository(getDB))
+                {
+                    return await repos.SubmitPurchaseData(request);
 
                 }
             }
