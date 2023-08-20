5dac34dd01cc0e4fb89cbdbf51bcb2be9c737cf6
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2023-08-20 23:33:25 +0530

    Add files via upload

--- CMMSAPIs/Models/SM/CMReports.cs
+++ CMMSAPIs/Models/SM/CMReports.cs
@@ -11,7 +11,7 @@ namespace CMMSAPIs.Models.SM
 
     public class CMPlantStockOpening
     {
-
+        public int facilityID {  get; set; }    
         public string facilityName { get; set; }
         public int Facility_Is_Block { get; set; }
         public string Facility_Is_Block_of_name { get; set; }
@@ -22,8 +22,11 @@ namespace CMMSAPIs.Models.SM
         public decimal Opening { get; set; }
         public decimal inward { get; set; }
         public decimal outward { get; set; }
+        public decimal balance { get; set; }
 
     }
+
+
 
     public class CMEmployeeStockReport
     {
@@ -38,6 +41,7 @@ namespace CMMSAPIs.Models.SM
         public decimal Opening { get; set; }
         public decimal inward { get; set; }
         public decimal outward { get; set; }
+        public decimal balance { get; set; }
 
     }
 
@@ -122,5 +126,26 @@ namespace CMMSAPIs.Models.SM
         public string item_name { get; set; }
         public decimal quantity { get; set; }
 
+    }
+
+    public class CMPlantStockOpeningResponse
+    {
+        public int facilityID { get; set; }
+        public string facilityName { get; set; }
+        public List<CMPlantStockOpeningItemWiseResponse> stockDetails { get; set; }
+    }
+
+    public class CMPlantStockOpeningItemWiseResponse
+    {
+        public int Facility_Is_Block { get; set; }
+        public string Facility_Is_Block_of_name { get; set; }
+        public int assetItemID { get; set; }
+        public string asset_name { get; set; }
+        public string asset_code { get; set; }
+        public int asset_type_ID { get; set; }
+        public decimal Opening { get; set; }
+        public decimal inward { get; set; }
+        public decimal outward { get; set; }
+        public decimal balance { get; set; }
     }
 }
