2a3ce73ff06bb3b2f181cb39474536331c2cdd89
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2023-09-25 22:52:16 +0530

    Update SessionMiddlerware.cs

--- CMMSAPIs/Middlewares/SessionMiddlerware.cs
+++ CMMSAPIs/Middlewares/SessionMiddlerware.cs
@@ -97,7 +97,7 @@ namespace CMMSAPIs.Middlewares
                     DataTable dt = new DataTable();
                     using (MySqlConnection conn = TheConnection)
                     {
-                        using (MySqlCommand cmd = getQryCommand("select * from userlog where userID = " + userId + " order by id desc;", conn))
+                        using (MySqlCommand cmd = getQryCommand("select * from userlog where userID = " + userId + " and customToken = "+ strlist[1] + " order by id desc;", conn))
                         {
                             await conn.OpenAsync();
                             cmd.CommandTimeout = 99999;
@@ -120,6 +120,7 @@ namespace CMMSAPIs.Middlewares
                     savedToken = Convert.ToString(dt.Rows[0]["customToken"]);
                     expiryTokenTime = Convert.ToDateTime(dt.Rows[0]["tokenExpiryTime"]);
                     tokenRefreshCount = Convert.ToInt32(dt.Rows[0]["tokenRefreshCount"]);
+           
 
                     // Check token validity
                     if (!savedToken.Equals(strlist[1]))
