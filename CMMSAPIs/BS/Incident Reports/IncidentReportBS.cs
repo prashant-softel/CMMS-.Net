17767491303243614f35f5d83e1cd135a2a4a3e1
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2024-01-13 01:00:33 +0530

    Update IncidentReportBS.cs

--- CMMSAPIs/BS/Incident Reports/IncidentReportBS.cs
+++ CMMSAPIs/BS/Incident Reports/IncidentReportBS.cs
@@ -1,4 +1,4 @@
-﻿using CMMSAPIs.Helper;
+using CMMSAPIs.Helper;
 using CMMSAPIs.Models.Incident_Reports;
 using CMMSAPIs.Models.Utils;
 using CMMSAPIs.Repositories.Incident_Reports;
@@ -12,10 +12,13 @@ namespace CMMSAPIs.BS.Incident_Reports
     {
         Task<List<CMIncidentList>> GetIncidentList(int facility_id, DateTime start_date, DateTime end_date);
         Task<CMDefaultResponse> CreateIncidentReport(CMCreateIncidentReport request,int userId);
+        Task<CMDefaultResponse> CreateIncidentInvestigationReport(CMCreateIncidentReport request,int userId);
         Task<CMViewIncidentReport> GetIncidentDetailsReport(int id);
         Task<CMDefaultResponse> UpdateIncidentReport(CMCreateIncidentReport request, int userId);
         Task<CMDefaultResponse> ApproveIncidentReport(int incidentId, int userId);
         Task<CMDefaultResponse> RejectIncidentReport(CMApproveIncident  request, int userId);
+        Task<CMDefaultResponse> ApproveIncidentInvestigationReport(int incidentId, int userId);
+        Task<CMDefaultResponse> RejectIncidentInvestigationReport(CMApproveIncident request, int userId);
 
     }
     public class IncidentReportBS : IIncidentReportBS
@@ -51,6 +54,23 @@ namespace CMMSAPIs.BS.Incident_Reports
                 using (var repos = new IncidentReportRepository(getDB))
                 {
                     return await repos.CreateIncidentReport(request,userId);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        public async Task<CMDefaultResponse> CreateIncidentInvestigationReport(CMCreateIncidentReport request, int userId)
+        {
+            try
+            {
+                using (var repos = new IncidentReportRepository(getDB))
+                {
+                    return await repos.CreateIncidentInvestigationReport(request, userId);
 
                 }
             }
@@ -117,6 +137,37 @@ namespace CMMSAPIs.BS.Incident_Reports
                 using (var repos = new IncidentReportRepository(getDB))
                 {
                     return await repos.RejectIncidentReport(request,userId);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<CMDefaultResponse> ApproveIncidentInvestigationReport(int incidentId, int userId)
+        {
+            try
+            {
+                using (var repos = new IncidentReportRepository(getDB))
+                {
+                    return await repos.ApproveIncidentInvestigationReport(incidentId, userId);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<CMDefaultResponse> RejectIncidentInvestigationReport(CMApproveIncident request, int userId)
+        {
+            try
+            {
+                using (var repos = new IncidentReportRepository(getDB))
+                {
+                    return await repos.RejectIncidentInvestigationReport(request, userId);
                 }
             }
             catch (Exception ex)
