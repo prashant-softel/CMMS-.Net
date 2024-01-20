f9aa50bee55f8acce5ee87bebcea964f1f9a1af1
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2024-01-20 13:04:47 +0530

    Update CMFacilityList.cs

--- CMMSAPIs/Models/Facility/CMFacilityList.cs
+++ CMMSAPIs/Models/Facility/CMFacilityList.cs
@@ -11,6 +11,7 @@ namespace CMMSAPIs.Models.Facility
         public string name { get; set; }
         public string spv { get; set; }
         public string address { get; set; }
+             public string description { get; set; }
         public string city { get; set; }
         public string state { get; set; }
         public string country { get; set; }
