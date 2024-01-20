dc893fbedebde218f5bd4076b25e1df54192bb92
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2024-01-20 13:07:22 +0530

    Update CMFacility.cs

--- CMMSAPIs/Models/Masters/CMFacility.cs
+++ CMMSAPIs/Models/Masters/CMFacility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,6 +24,7 @@ namespace CMMSAPIs.Models.Masters
         public string name { get; set; }
         public string spv { get; set; }
         public string address { get; set; }
+        public string description { get; set; }
         public string city { get; set; }
         public string state { get; set; }
         public string country { get; set; }
