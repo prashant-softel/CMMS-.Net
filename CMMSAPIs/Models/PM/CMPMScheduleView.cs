f6ec87a5b1a941214a16fd88ab0a793c688c5884
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2024-02-11 20:11:36 +0530

    Update CMPMScheduleView.cs

--- CMMSAPIs/Models/PM/CMPMScheduleView.cs
+++ CMMSAPIs/Models/PM/CMPMScheduleView.cs
@@ -1,4 +1,4 @@
-﻿using CMMSAPIs.Models.Utils;
+using CMMSAPIs.Models.Utils;
 using CMMSAPIs.Helper;
 using System;
 using System.Collections.Generic;
@@ -136,8 +136,8 @@ namespace CMMSAPIs.Models.PM
         public int is_custom_check_point { get; set; }
         public int is_file_required { get; set; }
         public int failure_score { get; set; }
-        public int min_range { get; set; }
-        public int max_range { get; set; }
+        public double min_range { get; set; }
+        public double max_range { get; set; }
     }
 
     public class ScheduleFiles
