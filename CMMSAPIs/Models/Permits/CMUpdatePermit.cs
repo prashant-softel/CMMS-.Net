602ce21fbce5a22fc9d030bd4f2c8e2177ea17bb
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2024-04-14 20:59:59 +0530

    Update CMUpdatePermit.cs

--- CMMSAPIs/Models/Permits/CMUpdatePermit.cs
+++ CMMSAPIs/Models/Permits/CMUpdatePermit.cs
@@ -35,5 +35,6 @@ namespace CMMSAPIs.Models.Permits
         public int TBT_Done_By { get; set; }
         public DateTime? TBT_Done_At  { get; set; }
         public List<CMPermitLotoOtherList> LotoOtherDetails { get; set; }
+        public List<int> uploadfile_ids { get; set; }
     }
 }
