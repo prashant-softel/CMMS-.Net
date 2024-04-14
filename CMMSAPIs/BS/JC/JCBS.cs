2a0b859e15b8a58935d74097394f9002dd096236
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2024-04-14 20:51:30 +0530

    Update JCBS.cs

--- CMMSAPIs/BS/JC/JCBS.cs
+++ CMMSAPIs/BS/JC/JCBS.cs
@@ -1,4 +1,4 @@
-﻿using CMMSAPIs.Helper;
+using CMMSAPIs.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +11,15 @@ namespace CMMSAPIs.BS.JC
 {
     public interface IJCBS
     {
-        Task<List<CMJCList>> GetJCList(int facility_id, int userID, bool self_view,string facilitytime);
-        Task<List<CMJCListForJob>> GetJCListByJobId(int jobId,string facilitytime);
+        Task<List<CMJCList>> GetJCList(int facility_id, int userID, bool self_view, string facilitytime);
+        Task<List<CMJCListForJob>> GetJCListByJobId(int jobId, string facilitytime);
         Task<List<CMJCDetail>> GetJCDetail(int jc_id);
         Task<CMDefaultResponse> CreateJC(int job_id, int userID);
         Task<CMDefaultResponse> UpdateJC(CMJCUpdate request, int userID);
         Task<CMDefaultResponse> CloseJC(CMJCClose request, int userID);
         Task<List<CMDefaultResponse>> ApproveJC(CMJCApprove request, int userID);
         Task<CMDefaultResponse> RejectJC(CMJCReject request, int userID);
-        Task<CMDefaultResponse> StartJC(int jc_id, int userID);
+        Task<CMDefaultResponse> StartJC(int jc_id, CMJCDetail request, int userID);
         Task<CMDefaultResponse> CarryForwardJC(CMApproval request, int userID);
         Task<CMDefaultResponse> RejectJCCF(CMJCReject request, int userID);
         Task<CMDefaultResponse> ApproveJCCF(CMJCApprove request, int userID);
@@ -40,7 +40,7 @@ namespace CMMSAPIs.BS.JC
             {
                 using (var repos = new JCRepository(getDB))
                 {
-                    return await repos.GetJCList(facility_id,  userID,  self_view, facilitytime);
+                    return await repos.GetJCList(facility_id, userID, self_view, facilitytime);
                 }
             }
             catch (Exception ex)
@@ -55,7 +55,7 @@ namespace CMMSAPIs.BS.JC
             {
                 using (var repos = new JCRepository(getDB))
                 {
-                    return await repos.GetJCListByJobId(jobId,  facilitytime);
+                    return await repos.GetJCListByJobId(jobId, facilitytime);
                 }
             }
             catch (Exception ex)
@@ -154,13 +154,13 @@ namespace CMMSAPIs.BS.JC
             }
         }
 
-        public async Task<CMDefaultResponse> StartJC(int jc_id, int userID)
+        public async Task<CMDefaultResponse> StartJC(int jc_id, CMJCDetail request, int userID)
         {
             try
             {
                 using (var repos = new JCRepository(getDB))
                 {
-                    return await repos.StartJC(jc_id, userID);
+                    return await repos.StartJC(jc_id, request, userID);
                 }
             }
             catch (Exception ex)
