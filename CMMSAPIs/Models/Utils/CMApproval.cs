62e1bde4a7c97b53d4d10b21201de84762d6a9f8
Prashantkuppili <67821636+Prashantkuppili@users.noreply.github.com>
2024-08-17 18:07:14 +0530

    Update CMApproval.cs

--- CMMSAPIs/Models/Utils/CMApproval.cs
+++ CMMSAPIs/Models/Utils/CMApproval.cs
@@ -6,6 +6,7 @@ namespace CMMSAPIs.Models.Utils
         public int id { get; set; }
         public string comment { get; set; }
         public int facility_id { get; set; }
+        public int claim_status { get; set; }
         public List<int> uploadfile_ids { get; set; }
 
     }
